995e899d8f9024cef0fa5f769087cfb103f9c404
sayanshanu61 <sayan.shanu61@gmail.com>
2020-02-25 00:35:19 +0530

    Add daily temperature high  & low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error){
             callback ('unable to find the location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degree out.  There is a ' + body.currently.precipProbability + ' % chance of rain.')
+            console.log(body.daily.data[0])
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degree out. This high today is ' + body.daily.data[0].temperatureHigh + ' degree with a low of ' + body.daily.data[0].temperatureLow + ' degree. There is a ' + body.currently.precipProbability + ' % chance of rain.')
 
             
         }
